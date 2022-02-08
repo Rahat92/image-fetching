b1bc77e53594f14205a42946ac10bc9144a72767
Kamrul Hasan <khrahat92@gmail.com>
2022-02-09 00:18:45 +0600

    modify image label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component{
             <div className='ui segment'>
                 <form onSubmit={this.onFormSubmit} className='ui form'>
                     <div className='field'>
-                        <label>Search for your picture</label>
+                        <label>Search for your desire picture</label>
                         <input
                          type="text" 
                          value={this.state.term} 
